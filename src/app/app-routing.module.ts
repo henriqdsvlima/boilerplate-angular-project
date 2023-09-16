9f78150832e9ef415816953141a3c16e907ef252
Henrique <henrique.dsvlima@gmail.com>
2023-09-16 10:47:10 -0300

    feature/routing - estruturando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent,
+    loadChildren: () => import('../app/app.module').then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
